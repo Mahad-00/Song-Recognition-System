91abbd79c033663645a186fd4ff60acd060a930f
Mahad Mustafa <mustafamahad03@gmail.com>
2026-07-06 16:27:03 +0500

    Use logo.png at 32x32 in sub-screens

--- frontend/src/screens/HistoryScreen.tsx
+++ frontend/src/screens/HistoryScreen.tsx
@@ -53,7 +53,7 @@ export default function HistoryScreen({ onNavigate }: Props) {
       <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 120 }}>
         {/* Header */}
         <View style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: 20, height: 64, marginTop: 24 }}>
-          <Image source={require('../images/logo.png')} style={{ width: 64, height: 64 }} resizeMode="contain" />
+          <Image source={require('../images/logo.png')} style={{ width: 32, height: 32 }} resizeMode="contain" />
         </View>
 
         <View style={{ paddingHorizontal: 20 }}>
